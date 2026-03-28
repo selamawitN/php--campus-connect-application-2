87eb02fe62232206709ad930e294d133048849ac
Tizita Endeshaw Yilma <tizita.endeshaw@aastustudent.edu.et>
2026-03-28 12:35:29 +0300

    Refactor db.php to remove duplicate connection logic
    Removed redundant database connection code.

--- backendpart/config/db.php
+++ backendpart/config/db.php
@@ -9,17 +9,4 @@ $conn = new mysqli($host, $user, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-    
-?>
-<?php
-$host = "localhost";
-$user = "root";
-$password = "";
-$dbname = "campus_connect";
-
-$conn = new mysqli($host, $user, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
 ?>
