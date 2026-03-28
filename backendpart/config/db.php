e409e15ee6aae08721caa734d56bcad32a98ac94
Tizita Endeshaw Yilma <tizita.endeshaw@aastustudent.edu.et>
2026-03-28 12:00:13 +0300

    Initialize database connection in db.php

--- backendpart/config/db.php
+++ backendpart/config/db.php
@@ -1,1 +1,12 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$dbname = "campus_connect";
 
+$conn = new mysqli($host, $user, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
