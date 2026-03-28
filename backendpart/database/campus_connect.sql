b419e58dd013ac18b89b541e5748a1b82b390a27
Tizita Endeshaw Yilma <tizita.endeshaw@aastustudent.edu.et>
2026-03-28 11:59:19 +0300

    Update campus_connect.sql

--- backendpart/database/campus_connect.sql
+++ backendpart/database/campus_connect.sql
@@ -1,1 +1,9 @@
-
+CREATE TABLE mentorship_requests (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    mentee_name VARCHAR(100) NOT NULL,
+    mentor_name VARCHAR(100) NOT NULL,
+    department VARCHAR(100) NOT NULL,
+    message TEXT NOT NULL,
+    status VARCHAR(50) DEFAULT 'pending',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
