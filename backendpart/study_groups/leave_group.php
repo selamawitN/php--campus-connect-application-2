22b6b375798e81094273262f34af99da708c812e
@tsedi16 <tsedey.fiseha@aastustudent.edu.et>
2026-03-29 21:00:23 +0300

    Update leave_group.php

--- backendpart/study_groups/leave_group.php
+++ backendpart/study_groups/leave_group.php
@@ -1,1 +1,37 @@
+<!DOCTYPE html>
+<html>
+<body>
 
+<h2>Leave Study Group</h2>
+
+<form method="POST">
+    Your Name: <input type="text" name="student_name"><br><br>
+
+    Select Group:
+    <select name="group">
+        <option>Math Group</option>
+        <option>Programming Group</option>
+        <option>Physics Group</option>
+    </select><br><br>
+
+    <input type="submit" value="Leave Group">
+</form>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $studentName = $_POST['student_name'];
+    $group = $_POST['group'];
+
+    if ($studentName != "") {
+        echo "<h3>You have left the group!</h3>";
+        echo "Student: " . $studentName . "<br>";
+        echo "Group: " . $group . "<br>";
+    } else {
+        echo "<p style='color:red;'>Enter your name!</p>";
+    }
+}
+?>
+
+</body>
+</html>
