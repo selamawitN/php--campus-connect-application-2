0d0b11455e8178a791da38369c8b9ea63ce88b13
@tsedi16 <tsedey.fiseha@aastustudent.edu.et>
2026-03-29 20:44:13 +0300

    create join_group.php

--- backendpart/study_groups/join_group.php
+++ backendpart/study_groups/join_group.php
@@ -1,1 +1,48 @@
+<!DOCTYPE html>
+<html>
+<body>
 
+<h2>Join Study Group</h2>
+
+<?php
+// Example groups (since you didn't learn DB yet)
+$groups = array(
+    "Math Group",
+    "Programming Group",
+    "Physics Group"
+);
+?>
+
+<form method="POST">
+    Your Name: <input type="text" name="student_name"><br><br>
+
+    Select Group:
+    <select name="group">
+        <?php
+        foreach ($groups as $g) {
+            echo "<option value='$g'>$g</option>";
+        }
+        ?>
+    </select><br><br>
+
+    <input type="submit" value="Join Group">
+</form>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $studentName = $_POST['student_name'];
+    $group = $_POST['group'];
+
+    if ($studentName != "") {
+        echo "<h3>Successfully Joined!</h3>";
+        echo "Student: " . $studentName . "<br>";
+        echo "Group: " . $group . "<br>";
+    } else {
+        echo "<p style='color:red;'>Enter your name!</p>";
+    }
+}
+?>
+
+</body>
+</html>
