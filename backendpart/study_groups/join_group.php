585d17a4a410966b63f34b050aca817866aec1f6
@tsedi16 <tsedey.fiseha@aastustudent.edu.et>
2026-03-29 21:24:29 +0300

    adding functionality to join_group.php

--- backendpart/study_groups/join_group.php
+++ backendpart/study_groups/join_group.php
@@ -5,44 +5,66 @@
 <h2>Join Study Group</h2>
 
 <?php
-// Example groups (since you didn't learn DB yet)
-$groups = array(
-    "Math Group",
-    "Programming Group",
-    "Physics Group"
-);
-?>
+$groups = array("Math Group", "Programming Group", "Physics Group");
 
-<form method="POST">
-    Your Name: <input type="text" name="student_name"><br><br>
+$nameErr = "";
+$studentName = "";
+$selectedGroup = "";
 
-    Select Group:
-    <select name="group">
-        <?php
-        foreach ($groups as $g) {
-            echo "<option value='$g'>$g</option>";
-        }
-        ?>
-    </select><br><br>
+// clean function
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
 
-    <input type="submit" value="Join Group">
-</form>
-
-<?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    $studentName = $_POST['student_name'];
-    $group = $_POST['group'];
-
-    if ($studentName != "") {
-        echo "<h3>Successfully Joined!</h3>";
-        echo "Student: " . $studentName . "<br>";
-        echo "Group: " . $group . "<br>";
+    if (empty($_POST["student_name"])) {
+        $nameErr = "Name is required";
     } else {
-        echo "<p style='color:red;'>Enter your name!</p>";
+        $studentName = test_input($_POST["student_name"]);
+
+        if (!preg_match("/^[a-zA-Z ]*$/", $studentName)) {
+            $nameErr = "Only letters allowed";
+        }
+    }
+
+    $selectedGroup = test_input($_POST["group"]);
+
+    if ($nameErr == "") {
+        echo "<h3>Joined Successfully!</h3>";
+        echo "Name: $studentName <br>";
+        echo "Group: $selectedGroup <br>";
     }
 }
 ?>
+
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+
+Your Name:
+<input type="text" name="student_name" value="<?php echo $studentName;?>">
+<span style="color:red;">* <?php echo $nameErr;?></span>
+<br><br>
+
+Select Group:
+<select name="group">
+<?php
+foreach ($groups as $g) {
+    if ($g == $selectedGroup) {
+        echo "<option selected>$g</option>";
+    } else {
+        echo "<option>$g</option>";
+    }
+}
+?>
+</select>
+<br><br>
+
+<input type="submit" value="Join Group">
+
+</form>
 
 </body>
 </html>
