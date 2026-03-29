0250b882dd74a141855670280c4ca2038749f646
@tsedi16 <tsedey.fiseha@aastustudent.edu.et>
2026-03-29 21:23:42 +0300

    adding functionality to create_group.php

--- backendpart/study_groups/create_group.php
+++ backendpart/study_groups/create_group.php
@@ -4,30 +4,78 @@
 
 <h2>Create Study Group</h2>
 
-<form method="POST">
-    Group Name: <input type="text" name="group_name"><br><br>
-    Subject: <input type="text" name="subject"><br><br>
-    Max Members: <input type="number" name="max_members"><br><br>
-    <input type="submit" value="Create Group">
-</form>
-
 <?php
+// error variables
+$nameErr = $subjectErr = $maxErr = "";
+$groupName = $subject = $maxMembers = "";
+
+// clean input function (from slides)
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    $groupName = $_POST['group_name'];
-    $subject = $_POST['subject'];
-    $maxMembers = $_POST['max_members'];
-
-    if ($groupName != "" && $subject != "" && $maxMembers != "") {
-        echo "<h3>Study Group Created!</h3>";
-        echo "Group Name: " . $groupName . "<br>";
-        echo "Subject: " . $subject . "<br>";
-        echo "Max Members: " . $maxMembers . "<br>";
+    // Group Name
+    if (empty($_POST["group_name"])) {
+        $nameErr = "Group name is required";
     } else {
-        echo "<p style='color:red;'>Please fill all fields!</p>";
+        $groupName = test_input($_POST["group_name"]);
+
+        // only letters and space
+        if (!preg_match("/^[a-zA-Z ]*$/", $groupName)) {
+            $nameErr = "Only letters allowed";
+        }
+    }
+
+    // Subject
+    if (empty($_POST["subject"])) {
+        $subjectErr = "Subject is required";
+    } else {
+        $subject = test_input($_POST["subject"]);
+    }
+
+    // Max Members
+    if (empty($_POST["max_members"])) {
+        $maxErr = "Required";
+    } else {
+        $maxMembers = test_input($_POST["max_members"]);
+    }
+
+    // if no errors
+    if ($nameErr == "" && $subjectErr == "" && $maxErr == "") {
+        echo "<h3>Study Group Created Successfully!</h3>";
+        echo "Group: $groupName <br>";
+        echo "Subject: $subject <br>";
+        echo "Max Members: $maxMembers <br>";
     }
 }
 ?>
+
+<!-- FORM -->
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+
+Group Name:
+<input type="text" name="group_name" value="<?php echo $groupName;?>">
+<span style="color:red;">* <?php echo $nameErr;?></span>
+<br><br>
+
+Subject:
+<input type="text" name="subject" value="<?php echo $subject;?>">
+<span style="color:red;">* <?php echo $subjectErr;?></span>
+<br><br>
+
+Max Members:
+<input type="number" name="max_members" value="<?php echo $maxMembers;?>">
+<span style="color:red;">* <?php echo $maxErr;?></span>
+<br><br>
+
+<input type="submit" value="Create Group">
+
+</form>
 
 </body>
 </html>
