cc9e7333d84e588f7ccd163c346781ff4fbccdb3
@tsedi16 <tsedey.fiseha@aastustudent.edu.et>
2026-03-29 21:25:27 +0300

    adding functionality to leave_group.php

--- backendpart/study_groups/leave_group.php
+++ backendpart/study_groups/leave_group.php
@@ -4,34 +4,69 @@
 
 <h2>Leave Study Group</h2>
 
-<form method="POST">
-    Your Name: <input type="text" name="student_name"><br><br>
-
-    Select Group:
-    <select name="group">
-        <option>Math Group</option>
-        <option>Programming Group</option>
-        <option>Physics Group</option>
-    </select><br><br>
-
-    <input type="submit" value="Leave Group">
-</form>
-
 <?php
+$nameErr = "";
+$studentName = "";
+$selectedGroup = "";
+
+// function (from slides)
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+$groups = array("Math Group", "Programming Group", "Physics Group");
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    $studentName = $_POST['student_name'];
-    $group = $_POST['group'];
-
-    if ($studentName != "") {
-        echo "<h3>You have left the group!</h3>";
-        echo "Student: " . $studentName . "<br>";
-        echo "Group: " . $group . "<br>";
+    if (empty($_POST["student_name"])) {
+        $nameErr = "Name is required";
     } else {
-        echo "<p style='color:red;'>Enter your name!</p>";
+        $studentName = test_input($_POST["student_name"]);
+
+        // only letters and spaces
+        if (!preg_match("/^[a-zA-Z ]*$/", $studentName)) {
+            $nameErr = "Only letters allowed";
+        }
+    }
+
+    $selectedGroup = test_input($_POST["group"]);
+
+    if ($nameErr == "") {
+        echo "<h3>You have successfully left the group!</h3>";
+        echo "Name: $studentName <br>";
+        echo "Group: $selectedGroup <br>";
     }
 }
 ?>
+
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+
+Your Name:
+<input type="text" name="student_name" value="<?php echo $studentName;?>">
+<span style="color:red;">* <?php echo $nameErr;?></span>
+<br><br>
+
+Select Group:
+<select name="group">
+<?php
+foreach ($groups as $g) {
+    if ($g == $selectedGroup) {
+        echo "<option selected>$g</option>";
+    } else {
+        echo "<option>$g</option>";
+    }
+}
+?>
+</select>
+
+<br><br>
+
+<input type="submit" value="Leave Group">
+
+</form>
 
 </body>
 </html>
