cad5703020e7acbbf8e4a093e22486557c727f61
@tsedi16 <tsedey.fiseha@aastustudent.edu.et>
2026-03-30 12:27:37 +0300

    Update db.php

--- backendpart/study_groups/config/db.php
+++ backendpart/study_groups/config/db.php
@@ -1,1 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = ""; // your MySQL root password
+$dbname = "campus_connect";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
 
