41fee64ecffd862c61c73f10f148faf934afbcae
@tsedi16 <tsedey.fiseha@aastustudent.edu.et>
2026-03-30 11:56:48 +0300

    connect creatgoup to database create_group.php

--- backendpart/study_groups/create_group.php
+++ backendpart/study_groups/create_group.php
@@ -1,81 +1,19 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<h2>Create Study Group</h2>
-
 <?php
-// error variables
-$nameErr = $subjectErr = $maxErr = "";
-$groupName = $subject = $maxMembers = "";
+include 'config/db.php';
 
-// clean input function (from slides)
-function test_input($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-}
+if(isset($_POST['group_name'])) {
+    $group_name = $conn->real_escape_string($_POST['group_name']);
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $sql = "INSERT INTO groups (group_name) VALUES ('$group_name')";
 
-    // Group Name
-    if (empty($_POST["group_name"])) {
-        $nameErr = "Group name is required";
+    if($conn->query($sql) === TRUE) {
+        echo "Group created successfully!";
     } else {
-        $groupName = test_input($_POST["group_name"]);
-
-        // only letters and space
-        if (!preg_match("/^[a-zA-Z ]*$/", $groupName)) {
-            $nameErr = "Only letters allowed";
-        }
-    }
-
-    // Subject
-    if (empty($_POST["subject"])) {
-        $subjectErr = "Subject is required";
-    } else {
-        $subject = test_input($_POST["subject"]);
-    }
-
-    // Max Members
-    if (empty($_POST["max_members"])) {
-        $maxErr = "Required";
-    } else {
-        $maxMembers = test_input($_POST["max_members"]);
-    }
-
-    // if no errors
-    if ($nameErr == "" && $subjectErr == "" && $maxErr == "") {
-        echo "<h3>Study Group Created Successfully!</h3>";
-        echo "Group: $groupName <br>";
-        echo "Subject: $subject <br>";
-        echo "Max Members: $maxMembers <br>";
+        echo "Error: " . $conn->error;
     }
 }
 ?>
-
-<!-- FORM -->
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-
-Group Name:
-<input type="text" name="group_name" value="<?php echo $groupName;?>">
-<span style="color:red;">* <?php echo $nameErr;?></span>
-<br><br>
-
-Subject:
-<input type="text" name="subject" value="<?php echo $subject;?>">
-<span style="color:red;">* <?php echo $subjectErr;?></span>
-<br><br>
-
-Max Members:
-<input type="number" name="max_members" value="<?php echo $maxMembers;?>">
-<span style="color:red;">* <?php echo $maxErr;?></span>
-<br><br>
-
-<input type="submit" value="Create Group">
-
+<form method="POST">
+    <input type="text" name="group_name" placeholder="Group Name" required>
+    <button type="submit">Create Group</button>
 </form>
-
-</body>
-</html>
