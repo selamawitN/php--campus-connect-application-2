7cb079247cb140067ed541b11573d574678e15e8
ornina dereje tefera <derejeornina@gmail.com>
2026-03-30 11:45:21 +0300

    Update get_resources.php

--- backendpart/resources/get_resources.php
+++ backendpart/resources/get_resources.php
@@ -1,1 +1,22 @@
+<?php
+$conn = new mysqli("localhost", "root", "", "campus_connect");
 
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM materials ORDER BY id DESC";
+$result = $conn->query($sql);
+
+$materials = [];
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $materials[] = $row;
+    }
+}
+
+echo json_encode($materials);
+
+$conn->close();
+?>
