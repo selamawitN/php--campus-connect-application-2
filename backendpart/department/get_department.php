b88ca989458e4eb58595dc79dc9529c0e79f1a0c
SEMIRA NESRU FEREJA <semira.nesru@aastustudent.edu.et>
2026-03-31 04:38:13 +0300

    Update get_department.php

--- backendpart/department/get_department.php
+++ backendpart/department/get_department.php
@@ -1,1 +1,26 @@
+<?php
+header("Content-Type: application/json");
+include_once "../config/db.php";
 
+$sql = "SELECT id, department_name, department_description FROM departments";
+$result = mysqli_query($conn, $sql);
+
+$departments = [];
+
+if ($result) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        $departments[] = $row;
+    }
+
+    echo json_encode([
+        "success" => true,
+        "message" => "Departments fetched successfully",
+        "data" => $departments
+    ]);
+} else {
+    echo json_encode([
+        "success" => false,
+        "message" => "Failed to fetch departments"
+    ]);
+}
+?>
