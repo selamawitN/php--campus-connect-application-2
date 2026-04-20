119ed611f3b0cf6f5ade8aba8d0ae7012522e710
selamawitN <selamawit.namssi@gmail.com>
2026-04-20 19:45:01 +0300

    Create delete_internship.php

---
+++ backendpart/internships/delete_internship.php
@@ -0,0 +1,17 @@
+<?php
+require_once '../config/db.php';
+session_start();
+
+if (!isset($_SESSION['user_id'])) {
+    header('Location: ../auth/login.php');
+    exit;
+}
+
+$id = $_GET['id'] ?? 0;
+$db = new Database();
+$conn = $db->getConnection();
+$stmt = $conn->prepare("DELETE FROM internships WHERE id=? AND posted_by=?");
+$stmt->execute([$id, $_SESSION['user_id']]);
+
+header('Location: my_internships.php?msg=deleted');
+?>
