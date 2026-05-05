08e142ef5176d308830e75ea3753cce668c597c0
TsionA363 <atsion989@gmail.com>
2026-05-05 07:34:46 +0300

    Create csrf_token.php

---
+++ backendpart/auth/csrf_token.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+header('conternt-Type: application/json');
+function gnerateCSRFToken(){
+  if(empty($_SESSSION['csrf_token']) ){
+    $_SESSION[csrf_token'] = bin2hex(randin_bytes(32));
+  }
+  return $_SESSION['csrf_token'];
+}
+function verifyCSRFToken($token){
+   return isset($_SESSION['csrf_token']) && hash_equals($_SESSION['csrf_token'], $token);
+}
+echo json_encode(['csrf_token' => generateCSRFToken()]);
+?>
