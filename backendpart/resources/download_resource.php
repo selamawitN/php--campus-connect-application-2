d8e04b1f56474ec5d1c300f2b38ac4734e5655b5
ornina dereje tefera <derejeornina@gmail.com>
2026-05-10 16:03:01 +0300

    Update download_resource.php

--- backendpart/resources/download_resource.php
+++ backendpart/resources/download_resource.php
@@ -1,1 +1,7 @@
-
+<?php
+$conn = new mysqli("localhost", "root", "", "campus_connect");
+$id = $_GET['id'] ?? 0;
+$conn->query("UPDATE materials SET downloads_count = downloads_count + 1 WHERE id = $id");
+$result = $conn->query("SELECT file_path, file_name FROM materials WHERE id = $id");
+$file = $result->fetch_assoc();
+//continue from here 
