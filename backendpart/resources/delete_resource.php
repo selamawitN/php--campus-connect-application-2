6515eb6231f34cd137901d925a156dc6b1104a08
ornina dereje tefera <derejeornina@gmail.com>
2026-05-10 15:56:29 +0300

    Update delete_resource.php

--- backendpart/resources/delete_resource.php
+++ backendpart/resources/delete_resource.php
@@ -1,1 +1,10 @@
+<?php
+sesiion_start();
+header('Content-Type: application/json');
+header('Access-control-Allow-Orign: *');
+include('../config/db.php");
+$input=json_decode(file_get_contents('php://input'),true);
+$id=$input['id'] ?? 0;
+//continue here..
+//the input has been a comment ask that
 
