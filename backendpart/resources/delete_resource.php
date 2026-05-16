5d98999e6806549bb5085198d6bc8dc06ed7c85f
ornina dereje tefera <derejeornina@gmail.com>
2026-05-16 19:39:24 +0300

    Update delete_resource.php

--- backendpart/resources/delete_resource.php
+++ backendpart/resources/delete_resource.php
@@ -7,4 +7,14 @@ $input=json_decode(file_get_contents('php://input'),true);
 $id=$input['id'] ?? 0;
 //continue here..
 //the input has been a comment ask that
+$result = $conn->query("SELECT file_path FROM materials WHERE id = $id");
+$file = $result->fetch_assoc();
 
+if ($file && file_exists($file['file_path'])) {
+    unlink($file['file_path']);
+}
+
+$conn->query("DELETE FROM materials WHERE id = $id");
+echo json_encode(['status' => 'success', 'message' => 'Deleted']);
+$conn->close();
+?>
